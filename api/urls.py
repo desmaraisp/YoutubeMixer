bf0f076ff579667624b4bea07e1769605edbd269
Philippe Desmarais <philippe.desmarais0trash@gmail.com>
2022-01-21 23:30:59 -0500

    Initial implementation of mock-up site

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import Get_Playlist_Contents
+from .views import Get_Combined_Playlist_Contents_From_Request
 
 urlpatterns = [
-    path("Get_Playlist_Contents", Get_Playlist_Contents, name="Get_Playlist_Contents"),
+    path("Get_Combined_Playlist_Contents_From_Request", Get_Combined_Playlist_Contents_From_Request, name="Get_Combined_Playlist_Contents_From_Request"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,74 @@
 import django.http as djh
+import googleapiclient.discovery
+import dpath.util as dpu
+import os
 
-def Get_Playlist_Contents(HttpRequest):
+
+def start_Google_API():
+	api_service_name = "youtube"
+	api_version = "v3"
+	DEVELOPER_KEY = os.environ["Youtube_API_Key"]
+	youtube = googleapiclient.discovery.build(
+		api_service_name, api_version, developerKey = DEVELOPER_KEY
+		)
+	return youtube
+	
+
+def Get_Single_PlayList_Contents(YoutubeObject,PlaylistID):
+	try:
+		result = YoutubeObject.playlistItems().list(
+				part="snippet",
+				playlistId=PlaylistID,
+				maxResults="50"
+			).execute()
+	except googleapiclient.discovery.HttpError as e:
+		return []
+
+	while ('nextPageToken' in result):
+		nextPage = YoutubeObject.playlistItems().list(
+			part="snippet",
+			playlistId=PlaylistID,
+			maxResults="50",
+			pageToken=result.get('nextPageToken')
+		).execute()
+		
+		result['items'] = result['items'] + nextPage['items']
+
+		if 'nextPageToken' not in nextPage:
+			result.pop('nextPageToken', None)
+		else:
+			nextPageToken = nextPage['nextPageToken']
+	
+	Result = []
+	video_IDs = dpu.values(result, "**/snippet/resourceId/videoId")
+
+	for i, ID in enumerate(video_IDs):
+		Result.append(
+			{
+				"ID":ID,
+				"Image":"Placeholder",
+				"Name":"Placeholder",
+			}
+		)
+	return Result
+
+
+
+def Get_Combined_Video_IDs_From_Playlists(Playlist_All_IDs):
+	Combined_Playlists_Video_IDs = []
+	
+	YoutubeObject = start_Google_API()
+	for PlayListID in Playlist_All_IDs:
+		Video_IDs_For_this_Playlist = Get_Single_PlayList_Contents(YoutubeObject, PlayListID)
+		Combined_Playlists_Video_IDs.extend(Video_IDs_For_this_Playlist)
+	
+	return Combined_Playlists_Video_IDs
+
+def Get_Combined_Playlist_Contents_From_Request(HttpRequest):
+	playlistIDs = HttpRequest.GET.getlist('PlaylistID')
+
+	Resulting_Video_Ids = Get_Combined_Video_IDs_From_Playlists(playlistIDs)
+
 	return djh.JsonResponse(
-		{"test":"testvalue"}
+		{"Contents":Resulting_Video_Ids}
 	)
