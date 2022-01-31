cdf416a63eb8b32a2f2149ad754f35fe8b559964
Philippe Desmarais <philippe.desmarais0trash@gmail.com>
2022-01-31 10:15:55 -0500

    Add sleep for testing purposes

--- start.sh
+++ start.sh
@@ -2,7 +2,8 @@
 set -e
 
 cd /app
+gunicorn --bind :8081 --workers 1 --threads 8 --timeout 1500 YoutubeMixer_project.wsgi &
 nginx -c "/etc/nginx/nginx.conf" &
-#gunicorn --bind :8081 --workers 1 --threads 8 --timeout 1500 YoutubeMixer_project.wsgi &
 
+sleep 20
 wait -n
