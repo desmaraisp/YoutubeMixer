2484ff8d0d613df597d2fd744b30ce5b68cc7c99
Philippe Desmarais <philippe.desmarais0trash@gmail.com>
2022-01-31 09:55:25 -0500

    Removed gunicorn for testing purposes

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ set -e
 
 cd /app
 nginx -c "/etc/nginx/nginx.conf" &
-gunicorn --bind :8081 --workers 1 --threads 8 --timeout 1500 YoutubeMixer_project.wsgi &
+#gunicorn --bind :8081 --workers 1 --threads 8 --timeout 1500 YoutubeMixer_project.wsgi &
 
 wait -n
