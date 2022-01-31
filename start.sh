206314ae5a7627633e00cf85ec6783fd9c5791db
Philippe Desmarais <philippe.desmarais0trash@gmail.com>
2022-01-31 10:48:13 -0500

    Disabled nginx for test

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@
 set -e
 
 cd /app
-gunicorn --bind :8081 --workers 1 --threads 8 --timeout 1500 YoutubeMixer_project.wsgi &
-nginx -c "/etc/nginx/nginx.conf" &
+gunicorn --bind :8080 --workers 1 --threads 8 --timeout 1500 YoutubeMixer_project.wsgi &
+#nginx -c "/etc/nginx/nginx.conf" &
 
 sleep 20
 wait -n
