52063bba1d122fe061a3e38fb0dc1d846ad2d420
Philippe Desmarais <philippe.desmarais0trash@gmail.com>
2022-01-31 22:17:04 -0500

    Increased logging level

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 set -e
 
 cd /app
-gunicorn --bind :8081 --workers 1 --threads 8 --timeout 1500 YoutubeMixer_project.wsgi &
+gunicorn --bind :8081 --workers 1 --threads 8 --log-level debug --timeout 1500 YoutubeMixer_project.wsgi &
 nginx -c "/etc/nginx/nginx.conf"
 
 
