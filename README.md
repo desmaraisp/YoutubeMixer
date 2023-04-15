f6fce6e84fcc8c5a7d061844a621ad1bdb0708f5
Philippe Desmarais <philippe.desmarais0trash@gmail.com>
2023-04-15 16:34:52 -0400

    feat: Create firestore database

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 initial setup: 
 gcloud projects create youtube-randomizer-1325234 --name="YoutubeRandomizer" + billing acc
+
+- create oauth for google + spotify refresh tokens
+- Set those in the application secrets
+
+post deploy: Enable google login in firebase
--- terraform/main-deployment/firebase.tf
+++ terraform/main-deployment/firebase.tf
@@ -56,23 +56,3 @@ resource "google_identity_platform_project_default_config" "auth" {
     google_identity_platform_config.auth
   ]
 }
-
-# resource "google_identity_platform_default_supported_idp_config" "idp_config" {
-#   enabled       = true
-#   idp_id        = "google.com"
-#   client_id     = 
-#   client_secret = 
-# }
-# resource "google_service_account" "firebase" {
-#   account_id   = "firebase"
-#   display_name = "FireBase Service Account"
-# }
-
-# resource "google_project_iam_member" "service_account_firebase_admin" {
-#   role   = "roles/editor"
-#   member = "serviceAccount:${google_service_account.firebase.email}"
-# }
-
-# resource "google_service_account_key" "firebase_key" {
-#   service_account_id = google_service_account.firebase.name
-# }
