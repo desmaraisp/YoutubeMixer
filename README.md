f68c861275e9f7d1c9835377876d7b27d090ae4f
Philippe Desmarais <philippe.desmarais0trash@gmail.com>
2023-04-15 17:05:11 -0400

    Added firestore rules to procedure

--- README.md
+++ README.md
@@ -4,4 +4,20 @@ gcloud projects create youtube-randomizer-1325234 --name="YoutubeRandomizer" + b
 - create oauth for google + spotify refresh tokens
 - Set those in the application secrets
 
-post deploy: Enable google login in firebase
+post deploy:
+- Enable google login in firebase
+- Set firestore rules:
+```
+rules_version = '2';
+service cloud.firestore {
+  match /databases/{database}/documents {
+    match /Player/{userId} {
+      allow read, update, delete, create: if request.auth != null && request.auth.uid == userId;
+    }
+    match /Playlists/{userId} {
+      allow read, update, delete, create: if request.auth != null && request.auth.uid == userId;
+    }
+
+  }
+}
+```
--- terraform/main-deployment/firestore.tf
+++ terraform/main-deployment/firestore.tf
@@ -16,37 +16,3 @@ resource "google_firestore_database" "database" {
 
   depends_on = [google_project_service.firestore]
 }
-
-resource "google_firebaserules_ruleset" "primary" {
-  provider = google-beta
-
-
-  source {
-    files {
-      content = <<EOF
-rules_version = '2';
-service cloud.firestore {
-  match /databases/{database}/documents {
-  	match /Authorizations {
-    	allow read, update, delete, create: if false
-    }
-  	
-    match /{collection}/{userId} {
-      allow read, update, delete, create: if request.auth != null && request.auth.uid == userId;
-    }
-  }
-}
-EOF
-      name    = "firestore.rules"
-    }
-  }
-
-  depends_on = [
-	google_firestore_database.database
-  ]
-}
-
-resource "google_firebaserules_release" "primary" {
-  name         = "release"
-  ruleset_name = google_firebaserules_ruleset.primary.name
-}
