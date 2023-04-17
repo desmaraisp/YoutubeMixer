74db5819ae7ae884e198b0794693d9124b346b6d
Philippe Desmarais <philippe.desmarais0trash@gmail.com>
2023-04-16 23:39:32 -0400

    fix: firebase admin sdk initialization error

--- src/firebase-admin-config.ts
+++ src/firebase-admin-config.ts
@@ -4,18 +4,20 @@ import { getFirestore } from 'firebase-admin/firestore'
 import firebaseAdmin from 'firebase-admin'
 import { applicationConfig } from "./configuration";
 
-
-const adminConfig: AppOptions = {
-	credential: credential.cert({
-		clientEmail: applicationConfig.clientEmail,
-		privateKey: applicationConfig.privateKey,
-		projectId: applicationConfig.publicProjectID
-	})
-}
-
-
 if (!firebaseAdmin.apps.length) {
-	firebaseAdmin.initializeApp(adminConfig);
+	if(!applicationConfig.clientEmail || !applicationConfig.privateKey){
+		firebaseAdmin.initializeApp()
+	}
+	else{
+		const adminConfig: AppOptions = {
+			credential: credential.cert({
+				clientEmail: applicationConfig.clientEmail,
+				privateKey: applicationConfig.privateKey,
+				projectId: applicationConfig.publicProjectID
+			})
+		}
+		firebaseAdmin.initializeApp(adminConfig);
+	}
 }
 export const adminAuth = getAuth(firebaseAdmin.app())
 export const adminDB = getFirestore(firebaseAdmin.app())
