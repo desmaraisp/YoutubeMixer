5c2b474465ffc31aa7c55fcb1ea40e375262f081
Philippe Desmarais <philippe.desmarais0trash@gmail.com>
2023-12-23 09:15:34 -0500

    chore: remove fake oidc provider

--- fake-oidc-provider/Pages/Account/Logout/LogoutOptions.cs
+++
@@ -1,8 +0,0 @@
-
-namespace fake_oidc_provider.Pages.Logout;
-
-public class LogoutOptions
-{
-    public static bool ShowLogoutPrompt = true;
-    public static bool AutomaticRedirectAfterSignOut = false;
-}
