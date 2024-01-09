9a69fd19b33f1fc1b3c10731efde8cd232e9227d
Philippe Desmarais <philippe.desmarais0trash@gmail.com>
2024-01-09 09:52:38 -0500

    fix: authentication error on / due to missing serversideprops

--- next-app/pages/index.tsx
+++ next-app/pages/index.tsx
@@ -1,4 +1,7 @@
 import { Text } from "@mantine/core";
+import { GetServerSidePropsContext } from "next";
+
+export const getServerSideProps = async (_context: GetServerSidePropsContext) => ({ props: { } })
 
 export default function Home() {
 	return (
