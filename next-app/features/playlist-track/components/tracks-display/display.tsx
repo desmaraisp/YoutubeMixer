30314a50823fadb69c825b827c38e5f8592c1925
Philippe Desmarais <philippe.desmarais0trash@gmail.com>
2024-01-16 23:25:17 -0500

    feat: add track type logo in tracks menu

--- next-app/features/playlist-track/components/tracks-display/display.tsx
+++ next-app/features/playlist-track/components/tracks-display/display.tsx
@@ -1,9 +1,11 @@
-import { PlaylistTrackModelWithId } from "@/features/playlist-track/playlist-track-schema";
-import { Alert, Card, ScrollArea, Table, useMantineTheme } from "@mantine/core";
+import { PlaylistTrackModelWithTrackType } from "@/features/playlist-track/playlist-track-schema";
+import { Alert, Card, ScrollArea, Table } from "@mantine/core";
 import { useContext } from "react";
 import { PlayerContext } from "../../../player/components/player-context-component/player-context";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpotify, faYoutube } from "@fortawesome/free-brands-svg-icons";
 
-export function TracksListDisplayTable({ tracks }: { tracks: PlaylistTrackModelWithId[] }) {
+export function TracksListDisplayTable({ tracks }: { tracks: PlaylistTrackModelWithTrackType[] }) {
 	const { currentTrackId, setCurrentTrackId } = useContext(PlayerContext)
 
 	if (tracks.length === 0) {
@@ -15,19 +17,18 @@ export function TracksListDisplayTable({ tracks }: { tracks: PlaylistTrackModelW
 			<Table withTableBorder withRowBorders>
 				<Table.Tbody>
 					{
-						tracks.map(x => (
-							<Table.Tr onClick={() => setCurrentTrackId(x.trackId)} style={{ backgroundColor: currentTrackId === x.trackId ? "blueviolet" : undefined }} key={x.trackId}>
+						tracks.map(x => {
+							const icon = x.trackType === 'Spotify' ? <FontAwesomeIcon icon={faSpotify}/> : <FontAwesomeIcon icon={faYoutube}/>
+
+							return <Table.Tr onClick={() => setCurrentTrackId(x.trackId)} style={{ backgroundColor: currentTrackId === x.trackId ? "blueviolet" : undefined }} key={x.trackId}>
 								<Table.Td>
-									Image
+									{icon}
 								</Table.Td>
 								<Table.Td>
 									{x.trackName}
 								</Table.Td>
-								<Table.Td>
-									{x.trackId}
-								</Table.Td>
 							</Table.Tr>
-						))
+						})
 					}
 				</Table.Tbody>
 			</Table>
