72106f7656530fb05a17ea86c239355ad907865a
Philippe Desmarais <philippe.desmarais0trash@gmail.com>
2024-11-09 21:34:35 -0500

    feat: add uri-based authorization filter attribute
    This filter serves for our cases where we have a userId in our route params and we want to ascertain that said userId matches the user's claims

---
+++ src/Randomizer.Api/Features/Auth/UserIdAuthorizationFilterAttribute.cs
@@ -0,0 +1,27 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Randomizer.Api.Features.Auth;
+
+[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+public class UserIdAuthorizationFilterAttribute : Attribute, IAsyncAuthorizationFilter
+{
+	public Task OnAuthorizationAsync(AuthorizationFilterContext context)
+	{
+		var userIdInUrl = context.RouteData.Values["userId"]?.ToString();
+
+		if(string.IsNullOrWhiteSpace(userIdInUrl)){
+			context.Result = new UnauthorizedObjectResult("No userId found in uri");
+			return Task.CompletedTask;
+		}
+		var userId = context.HttpContext.User.Identities.First().Claims.Single(x => x.Type == ClaimTypes.NameIdentifier).Value;
+		if (userIdInUrl != userId)
+		{
+			context.Result = new UnauthorizedObjectResult("UserId in uri does not match userId in claims");
+			return Task.CompletedTask;
+		}
+
+		return Task.CompletedTask;
+	}
+}
