18b211805e7fb5208305983d11c3b95fe97d21f8
Philippe Desmarais <philippe.desmarais0trash@gmail.com>
2024-11-09 21:31:32 -0500

    tests: add tests project for future test additions

---
+++ src/Randomizer.Api.Tests/ConfigureSqlite.cs
@@ -0,0 +1,60 @@
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Logging;
+using Randomizer.Dal;
+
+namespace Randomizer.Api.Tests;
+
+public static class ConfigureSqLite
+{
+	private readonly static ILoggerFactory fact = LoggerFactory.Create(x => x.AddConsole());
+	public static RandomizerContext ConfigureRandomizerContext()
+	{
+		SqliteConnection connection = CreateConnection();
+
+		DbContextOptions<RandomizerContext> contextOptions = new DbContextOptionsBuilder<RandomizerContext>()
+			.UseLoggerFactory(fact)
+			.UseSqlite(connection)
+			.ReplaceService<IModelCustomizer, SqliteModelCustomizer>()
+			.Options;
+
+		var RandomizerContext = new RandomizerContext(contextOptions);
+		RandomizerContext.Database.EnsureCreated();
+		return RandomizerContext;
+	}
+
+	private static SqliteConnection CreateConnection()
+	{
+		SqliteConnection connection = new("Filename=:memory:");
+		connection.Open();
+		return connection;
+	}
+}
+
+internal class SqliteModelCustomizer : IModelCustomizer
+{
+	public void Customize(ModelBuilder modelBuilder, DbContext context)
+	{
+		modelBuilder.Entity<Player>(c =>
+		{
+			c.Property(x => x.Id).ValueGeneratedNever();
+		});
+		modelBuilder.Entity<PlayerPlaylist>(c =>
+		{
+			c.Property(x => x.Id).ValueGeneratedNever();
+		});
+		modelBuilder.Entity<PlaylistTrack>(c =>
+		{
+			c.Property(x => x.Id).ValueGeneratedNever();
+		});
+		modelBuilder.Entity<TrackWeight>(c =>
+		{
+			c.Property(x => x.Id).ValueGeneratedNever();
+		});
+		modelBuilder.Entity<PlayerProgress>(c =>
+		{
+			c.Property(x => x.Id).ValueGeneratedNever();
+		});
+	}
+}
