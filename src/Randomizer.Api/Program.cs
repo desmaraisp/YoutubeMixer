0de4b31bfea248a1ee417c6f78612e3d99615eec
Philippe Desmarais <philippe.desmarais0trash@gmail.com>
2024-11-22 01:41:38 -0500

    chore: replace swagger openapi generator with Microsoft.AspNetCore.OpenApi

--- src/Randomizer.Api/Program.cs
+++ src/Randomizer.Api/Program.cs
@@ -36,16 +36,12 @@ internal sealed class Program
 
 		builder.Services.AddControllers();
 		builder.Services.AddEndpointsApiExplorer();
-		builder.Services.AddSwaggerGen();
+		builder.Services.AddOpenApi();
 
 		var app = builder.Build();
-		
-		if (app.Environment.IsDevelopment())
-		{
-			app.UseSwagger();
-			app.UseSwaggerUI();
-		}
 
+		app.MapOpenApi();
+		app.UseSwaggerUI();
 		app.UseAuthorization();
 
 		var rootApiVersionSet = app.NewApiVersionSet()
